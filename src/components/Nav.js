64f0b1c1ca174cea958ea327ee9b674a0df9e143
mone <qhdltmwhs@gmail.com>
2025-06-15 07:20:42 +0900

    Swiper add to Row.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,46 +3,50 @@ import "./Nav.css";
 import { useNavigate } from "react-router-dom";
 
 export default function Nav() {
-    const [show, setShow] = useState(false);
-    const [searchValue, setSearchValue] = useState("");
-    const navigate = useNavigate();
+  const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
-    useEffect(() => {
-      window.addEventListener("scroll",() => {
-        console.log(`window.scrollY, ${window.scrollY}`);
-        if(window.scrollY > 50) {
-            setShow(true);
-        } else {
-            setShow(false);
-        }
-      });
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      // console.log(`window.scrollY, ${window.scrollY}`);
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
 
-      return () => {
-        window.removeEventListener("scroll", () => {});
-      };
-    }, []);
-
-    const handleChange = (e) => {
-      setSearchValue(e.target.value);
-      navigate(`/search?q=${e.target.value}`);
+    return () => {
+      window.removeEventListener("scroll", () => {});
     };
-    
-    return (
+  }, []);
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
+  return (
     <nav className={`nav ${show && "nav__black"}`}>
-      <img 
+      <img
         alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
         className="nav__logo"
-        onClick={() => window.location.reload()}
+        onClick={() => {
+          setSearchValue(""); // 입력값 초기화
+          navigate("/", { replace: true }); // 메인 이동 + 히스토리 정리
+        }}
+        // onClick={() => window.location.reload()} // 기존
       />
-      <input 
+      <input
         className="nav__input"
         type="text"
         placeholder="영화를 검색해주세요."
-        value={searchValue} 
+        value={searchValue}
         onChange={handleChange}
       />
-      <img 
+      <img
         alt="User logged"
         src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
         className="nav__avatar"
